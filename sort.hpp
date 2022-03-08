1763e94c7f6c386eeb0720c78ea5f0d1b66e4330
ArminGh02 <arminghorbanian02@gmail.com>
2022-03-08 11:59:07 +0330

    Write signatures of some functions

---
+++ sort.hpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+namespace alg {
+
+template<typename RandomAccessIterator, typename Compare>
+void bubble_sort(RandomAccessIterator first,
+                 RandomAccessIterator last,
+                 Compare comp = std::less<typename RandomAccessIterator::value_type>()) {
+    
+}
+
+template<typename RandomAccessIterator, typename Compare>
+void insertion_sort(RandomAccessIterator first,
+                    RandomAccessIterator last,
+                    Compare comp = std::less<typename RandomAccessIterator::value_type>()) {
+
+}
+
+template<typename RandomAccessIterator, typename Compare>
+void insertion_sort_recursive(RandomAccessIterator first,
+                              RandomAccessIterator last,
+                              Compare comp = std::less<typename RandomAccessIterator::value_type>()) {
+
+}
+
+template<typename RandomAccessIterator, typename Compare>
+void merge_sort(RandomAccessIterator first,
+                RandomAccessIterator last,
+                Compare comp = std::less<typename RandomAccessIterator::value_type>()) {
+
+}
+
+template<typename RandomAccessIterator, typename Compare>
+void quick_sort(RandomAccessIterator first,
+                RandomAccessIterator last,
+                Compare comp = std::less<typename RandomAccessIterator::value_type>()) {
+
+}
+
+template<typename RandomAccessIterator, typename Compare>
+void counting_sort(RandomAccessIterator first,
+                   RandomAccessIterator last,
+                   Compare comp = std::less<typename RandomAccessIterator::value_type>()) {
+
+}
+
+template<typename RandomAccessIterator, typename Compare>
+void radix_sort(RandomAccessIterator first,
+                RandomAccessIterator last,
+                Compare comp = std::less<typename RandomAccessIterator::value_type>()) {
+
+}
+
+template<typename RandomAccessIterator, typename Compare>
+void bucket_sort(RandomAccessIterator first,
+                 RandomAccessIterator last,
+                 Compare comp = std::less<typename RandomAccessIterator::value_type>()) {
+
+}
+
+}
