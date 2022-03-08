44de5decbc3f31d1d607dda3bdaa0641189fdd93
ArminGh02 <arminghorbanian02@gmail.com>
2022-03-08 17:47:27 +0330

    Fix the mistake of forgetting to increment the iterator of for loop

--- sort.hpp
+++ sort.hpp
@@ -7,7 +7,7 @@ void bubble_sort(RandomAccessIterator first,
                  RandomAccessIterator last,
                  Compare comp = std::less<typename RandomAccessIterator::value_type>()) {
     for (; first != last - 1; ++first) {
-        for (auto it = first + 1; it != last;) {
+        for (auto it = first + 1; it != last; ++it) {
             if (comp(*it, *first)) {
                 std::iter_swap(it, first);
             }
