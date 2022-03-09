35a1f9c34c14595925acddeb7f28d4fcdc3a5e7a
ArminGh02 <arminghorbanian02@gmail.com>
2022-03-09 03:51:38 +0330

    Write merge function

---
+++ algorithms.hpp
@@ -0,0 +1,36 @@
+#include <vector>
+
+namespace alg {
+
+template<typename RandomAccessIterator, typename Compare>
+void merge(RandomAccessIterator first,
+           RandomAccessIterator mid,
+           RandomAccessIterator last,
+           Compare comp = std::less<typename RandomAccessIterator::value_type>()) {
+    std::vector<typename RandomAccessIterator::value_type> temp;
+    temp.reserve(std::distance(first, last) + 1);
+    auto left = first;
+    auto right = mid;
+    while (left < mid && right <= last) {
+        if (comp(*right, *left)) {
+            temp.push_back(*right);
+            ++right;
+        } else {
+            temp.push_back(*left);
+            ++left;
+        }
+    }
+
+    while (left <= mid - 1) {
+        temp.push_back(*left);
+        ++left;
+    }
+    while (right < last) {
+        temp.push_back(*right);
+        ++right;
+    }
+
+    std::copy(temp.begin(), temp.end(), first);
+}
+
+}  // namespace alg
