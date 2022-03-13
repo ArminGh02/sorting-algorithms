2f8ce217ee65773d7635bb6da21b0a90c561e48b
ArminGh02 <arminghorbanian02@gmail.com>
2022-03-13 18:00:53 +0330

    Write all partitioning functions

--- algorithms.hpp
+++ algorithms.hpp
@@ -1,6 +1,8 @@
 #ifndef ALGORITHMS_HPP
 #define ALGORITHMS_HPP
 
+#include <random>
+#include <chrono>
 #include <vector>
 
 namespace alg {
@@ -37,33 +39,70 @@ void merge(const RandomAccessIterator first,
     std::copy(temp.begin(), temp.end(), first);
 }
 
-template<typename RandomAccessIterator, typename Compare>
-RandomAccessIterator selection(RandomAccessIterator first,
-                               RandomAccessIterator last,
-                               typename RandomAccessIterator::difference_type k,
-                               Compare comp = std::less<typename RandomAccessIterator::value_type>()) {
+template<typename BidirectionalIterator, typename Compare>
+BidirectionalIterator partition(
+    BidirectionalIterator first,
+    BidirectionalIterator last,
+    BidirectionalIterator pivot,
+    Compare comp = std::less<typename BidirectionalIterator::value_type>()
+) {
+    auto e = last;
+    --e;
+    std::iter_swap(pivot, e);
 
-}
-
-template<typename RandomAccessIterator, typename Compare>
-RandomAccessIterator partition(RandomAccessIterator first,
-                               RandomAccessIterator last,
-                               Compare comp = std::less<typename RandomAccessIterator::value_type>()) {
-    auto median = selection(first, last, std::distance(first, last) / 2, comp);
-
-    std::iter_swap(median, last - 1);
-
-    auto iter = first - 1;
+    auto iter = first;
+    --iter;
     for (; first != last; ++first) {
-        if (comp(*first, *median)) {
+        if (comp(*first, *pivot)) {
             ++iter;
             std::iter_swap(*first, *iter);
         }
     }
 
     ++iter;
-    std::iter_swap(--last, iter);
+    std::iter_swap(e, iter);
     return iter;
+}
+
+template<typename BidirectionalIterator, typename Compare>
+BidirectionalIterator partition_pivot_last(
+    const BidirectionalIterator first,
+    const BidirectionalIterator last,
+    Compare comp = std::less<typename BidirectionalIterator::value_type>()
+) {
+    return partition(first, last, --last, comp);
+}
+
+template<typename RandomAccessIterator, typename Compare>
+RandomAccessIterator partition_random(
+    const RandomAccessIterator first,
+    const RandomAccessIterator last,
+    Compare comp = std::less<typename RandomAccessIterator::value_type>()
+) {
+    static std::mt19937 gen(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+    std::uniform_int_distribution<> dist(0, std::distance(first, last));
+    auto pivot = first + dist(gen);
+    return partition(first, last, pivot, comp);
+}
+
+template<typename RandomAccessIterator, typename Compare>
+RandomAccessIterator selection(
+    const RandomAccessIterator first,
+    const RandomAccessIterator last,
+    typename RandomAccessIterator::difference_type k,
+    Compare comp = std::less<typename RandomAccessIterator::value_type>()
+) {
+
+}
+
+template<typename RandomAccessIterator, typename Compare>
+RandomAccessIterator partition_median(
+    const RandomAccessIterator first,
+    const RandomAccessIterator last,
+    Compare comp = std::less<typename RandomAccessIterator::value_type>()
+) {
+    auto pivot = selection(first, last, std::distance(first, last) / 2, comp);
+    return partition(first, last, pivot, comp)
 }
 
 }  // namespace alg
