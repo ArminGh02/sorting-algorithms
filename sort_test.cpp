e838a8e61984c25a2124bb488d96d6d95b2e79e5
ArminGh02 <arminghorbanian02@gmail.com>
2022-03-19 12:42:00 +0330

    Write test case for `radix_sort` & `counting_sort`

--- sort_test.cpp
+++ sort_test.cpp
@@ -8,10 +8,10 @@
 
 #include "sort.hpp"
 
-TEST_CASE("sorting algorithms functions int") {
-    std::mt19937 gen(std::chrono::high_resolution_clock::now().time_since_epoch().count());
-    std::uniform_int_distribution<> dist;
+static std::mt19937 gen(std::chrono::high_resolution_clock::now().time_since_epoch().count());
 
+TEST_CASE("sorting algorithms functions for ints") {
+    std::uniform_int_distribution<> dist;
     constexpr std::size_t ELEMENTS_COUNT = 500000;
 
     std::vector<int> to_sort;
@@ -82,5 +82,31 @@ TEST_CASE("sorting algorithms functions int") {
             alg::heap_sort(to_sort.begin(), to_sort.end(), std::greater<int>());
             REQUIRE(std::is_sorted(to_sort.begin(), to_sort.end(), std::greater<int>()));
         }
+    }
+}
+
+TEST_CASE("radix_sort & counting_sort") {
+    constexpr auto MAX_ELEMENT = 100000U;
+    std::uniform_int_distribution<unsigned> dist(0U, MAX_ELEMENT);
+
+    constexpr std::size_t ELEMENTS_COUNT = 500000;
+
+    std::vector<unsigned> to_sort;
+    to_sort.reserve(ELEMENTS_COUNT);
+
+    for (std::size_t i = 0; i < ELEMENTS_COUNT; ++i) {
+        to_sort.push_back(dist(gen));
+    }
+
+    REQUIRE(to_sort.size() == ELEMENTS_COUNT);
+    REQUIRE(*std::max_element(to_sort.begin(), to_sort.end()) <= MAX_ELEMENT);
+
+    SECTION("counting_sort") {
+        alg::counting_sort(to_sort.begin(), to_sort.end(), MAX_ELEMENT);
+        REQUIRE(std::is_sorted(to_sort.begin(), to_sort.end()));
+    }
+    SECTION("radix_sort") {
+        alg::radix_sort(to_sort.begin(), to_sort.end(), MAX_ELEMENT);
+        REQUIRE(std::is_sorted(to_sort.begin(), to_sort.end()));
     }
 }
