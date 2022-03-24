e89fcb38dff437c09e894c6632d871f1cbd73036
ArminGh02 <arminghorbanian02@gmail.com>
2022-03-24 12:50:57 +0430

    Write benchmark for `radix_sort` and `counting_sort`

--- sort_benchmark.cpp
+++ sort_benchmark.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <chrono>
+#include <limits>
 #include <random>
 #include <unordered_map>
 #include <vector>
@@ -15,6 +16,9 @@ struct SortFunc { enum type {
     heap_sort,
     merge_sort,
     quick_sort,
+    counting_sort,
+    radix_sort,
+    bucket_sort,
     std_sort,
     std_stable_sort,
 }; };
@@ -32,30 +36,33 @@ static std::unordered_map<SortFunc::type, FuncPtr> func_map = {
     { SortFunc::std_stable_sort, [](auto first, auto last) { std::stable_sort(first, last); } },
 };
 
-static std::vector<int> random_int_vector(std::size_t size) {
+template<typename Int>
+static std::vector<Int> random_int_vector(std::size_t size, Int max = std::numeric_limits<Int>::max()) {
     std::mt19937 gen(std::chrono::high_resolution_clock::now().time_since_epoch().count());
-    std::uniform_int_distribution<> dist;
+    std::uniform_int_distribution<Int> dist(std::numeric_limits<Int>::min(), max);
 
-    std::vector<int> vec(size);
-    std::generate(vec.begin(), vec.end(), [&gen, &dist]() -> int { return dist(gen); });
+    std::vector<Int> vec(size);
+    std::generate(vec.begin(), vec.end(), [&gen, &dist]() -> Int { return dist(gen); });
 
     return vec;
 }
 
-static std::vector<int> sorted_random_int_vector(std::size_t size) {
-    auto vec = random_int_vector(size);
+template<typename Int>
+static std::vector<Int> sorted_random_int_vector(std::size_t size, Int max = std::numeric_limits<Int>::max()) {
+    auto vec = random_int_vector(size, max);
     std::sort(vec.begin(), vec.end());
     return vec;
 }
 
-static std::vector<int> reverse_sorted_random_int_vector(std::size_t size) {
-    auto vec = random_int_vector(size);
+template<typename Int>
+static std::vector<Int> reverse_sorted_random_int_vector(std::size_t size, Int max = std::numeric_limits<Int>::max()) {
+    auto vec = random_int_vector(size, max);
     std::sort(vec.rbegin(), vec.rend());
     return vec;
 }
 
 static void bm_sort_random_vector(benchmark::State& state) {
-    static auto vec = random_int_vector(10000);
+    static auto vec = random_int_vector<int>(10000);
     for (auto _ : state) {
         state.PauseTiming();
         auto tmp = vec;
@@ -68,7 +75,7 @@ static void bm_sort_random_vector(benchmark::State& state) {
 }
 
 static void bm_sort_sorted_vector(benchmark::State& state) {
-    static auto vec = sorted_random_int_vector(10000);
+    static auto vec = sorted_random_int_vector<int>(10000);
     for (auto _ : state) {
         state.PauseTiming();
         auto tmp = vec;
@@ -81,7 +88,7 @@ static void bm_sort_sorted_vector(benchmark::State& state) {
 }
 
 static void bm_sort_reverse_sorted_vector(benchmark::State& state) {
-    static auto vec = reverse_sorted_random_int_vector(10000);
+    static auto vec = reverse_sorted_random_int_vector<int>(10000);
     for (auto _ : state) {
         state.PauseTiming();
         auto tmp = vec;
@@ -90,6 +97,36 @@ static void bm_sort_reverse_sorted_vector(benchmark::State& state) {
         state.ResumeTiming();
 
         sort_func(tmp.begin(), tmp.end());
+    }
+}
+
+static void bm_counting_sort_and_radix_sort(benchmark::State& state) {
+    constexpr auto MAX = 1000U;
+    static auto vec = random_int_vector(10000, MAX);
+    for (auto _ : state) {
+        state.PauseTiming();
+
+        auto func = static_cast<SortFunc::type>(state.range(0));
+        auto tmp = vec;
+
+        switch (func) {
+        case SortFunc::counting_sort:
+            state.ResumeTiming();
+            alg::counting_sort(tmp.begin(), tmp.end(), MAX);
+            break;
+        case SortFunc::radix_sort:
+            state.ResumeTiming();
+            alg::radix_sort(tmp.begin(), tmp.end(), MAX);
+            break;
+        case SortFunc::std_sort:
+            state.ResumeTiming();
+            std::sort(tmp.begin(), tmp.end());
+            break;
+        case SortFunc::std_stable_sort:
+            state.ResumeTiming();
+            std::stable_sort(tmp.begin(), tmp.end());
+            break;
+        }
     }
 }
 
@@ -120,6 +157,12 @@ BENCHMARK(bm_sort_reverse_sorted_vector)
     ->Arg(SortFunc::heap_sort)
     ->Arg(SortFunc::merge_sort)
     ->Arg(SortFunc::quick_sort)
+    ->Arg(SortFunc::std_sort)
+    ->Arg(SortFunc::std_stable_sort);
+
+BENCHMARK(bm_counting_sort_and_radix_sort)
+    ->Arg(SortFunc::counting_sort)
+    ->Arg(SortFunc::radix_sort)
     ->Arg(SortFunc::std_sort)
     ->Arg(SortFunc::std_stable_sort);
 
